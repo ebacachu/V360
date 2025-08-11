2265e1966b9ae84b39242c471761b84b1e6efaf8
ebacachu <ebuenoalfaro@gmail.com>
2025-08-11 10:05:05 +0200

    cc

--- data.js
+++ data.js
@@ -1,8 +1,8 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-dji_20250810205037_0866_d",
-      "name": "DJI_20250810205037_0866_D",
+      "id": "0-embalse",
+      "name": "Embalse",
       "levels": [
         {
           "tileSize": 256,
@@ -28,17 +28,66 @@ var APP_DATA = {
       ],
       "faceSize": 3000,
       "initialViewParameters": {
-        "yaw": 0.09001899317355999,
-        "pitch": 0.283880444796317,
-        "fov": 1.38217411905719
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
       },
-      "linkHotspots": [],
+      "linkHotspots": [
+        {
+          "yaw": -0.04105876923476437,
+          "pitch": 0.2350443328923859,
+          "rotation": 3.141592653589793,
+          "target": "1-presa"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-presa",
+      "name": "Presa",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.28056960724871693,
+          "pitch": 0.5541572444544673,
+          "rotation": 0,
+          "target": "0-embalse"
+        }
+      ],
       "infoHotspots": []
     }
   ],
   "name": "Project Title",
   "settings": {
-    "mouseViewMode": "qtvr",
+    "mouseViewMode": "drag",
     "autorotateEnabled": false,
     "fullscreenButton": false,
     "viewControlButtons": false
